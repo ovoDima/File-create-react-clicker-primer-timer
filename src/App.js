f81a5cf88d02f26cd224cc80ece67daa75d1f4d0
ovodima <dima.tsymbal12345@gmail.com>
2021-06-11 14:05:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Display from './Components/Display';
+import  Button  from './Components/Button';
+
+
 
 function App() {
+const [time, setTime] = useState({ms:0, s:0, m:0, h:0})
+const [interv, setInterv] = useState()
+const [status, setStatus] = useState(0)
+
+const start = () => {
+  run()
+  setStatus(1)
+  setInterv(setInterval(run, 10))
+}
+
+let updateMs = time.ms, updateS = time.s, updateM = time.m, updateH = time.h
+
+const run = () => {
+  if(updateM === 60) {
+    updateH++
+    updateM = 0
+  }
+  if(updateS === 60) {
+    updateM++
+    updateS = 0
+  }
+  if(updateMs === 100) {
+    updateS++
+    updateMs = 0
+  }
+  updateMs++
+  return setTime({ms:updateMs, s:updateS, m:updateM, h: updateH})
+}
+
+const stop = () => {
+  clearInterval(interv)
+  setStatus(2)
+}
+
+const reset = () => {
+  clearInterval(interv)
+  setStatus(0)
+  setTime({ms:0, s:0, m:0, h:0})
+}
+
+const resume = () => start()
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          <Display time = {time} />
+          <Button 
+          start = {start} 
+          status = {status} 
+          resume = {resume}
+          reset = {reset} 
+          stop = {stop}
+          />
+        </div>
+      </div>
     </div>
+    
+    
   );
 }
 
